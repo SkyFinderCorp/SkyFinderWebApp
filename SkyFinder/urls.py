9b0b41c92dea5d42730490ee1602b28a8f577977
Mat4539 <mborg0020@gmail.com>
2023-04-04 12:26:21 -0400

    Created Html File & Setup Roughts for home page

--- SkyFinder/urls.py
+++ SkyFinder/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from SkyFinderWebApp.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home_page'),
 ]
