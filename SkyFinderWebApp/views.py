229423405b54a2a3571251d0afc6af1f4a61cf69
Mat4539 <mborg0020@gmail.com>
2023-04-11 21:10:47 -0400

    Added API Call With Rest API & Created HTML Form for it

--- SkyFinderWebApp/views.py
+++ SkyFinderWebApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+import array as arr
 from django.shortcuts import render
+import requests
 
 
 def home(request):
@@ -8,6 +10,43 @@ def home(request):
 
 
 def track(request):
+    global flight_data
+    if request.method == "POST":
+        form_data = request.POST
+
+        air_line = form_data['airline']
+        flight_number = form_data['flightnumber']
+
+        params = {
+            'access_key': 'c16bd46c7e14a4196a0fa5a328f7866f',
+            'airline_name': air_line,
+            'flight_iata': flight_number
+        }
+
+        results = requests.get('http://api.aviationstack.com/v1/flights', params=params)
+
+        api_response = results.json()
+
+        for flight in api_response['data']:
+            flight_data = [
+                flight['airline']['name'],
+                flight['flight']['number'],
+                flight['flight']['iata'],
+
+                flight['departure']['airport'],
+                flight['departure']['iata'],
+                flight['departure']['estimated'],
+                flight['departure']['gate'],
+                flight['departure']['terminal'],
+
+                flight['arrival']['airport'],
+                flight['arrival']['iata'],
+                flight['arrival']['estimated'],
+                flight['arrival']['gate'],
+                flight['arrival']['terminal']
+            ]
+
+        return render(request, 'track.html', {'flightData': flight_data})
     return render(request, "track.html")
 
 
