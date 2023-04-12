abd8eecebe96c701788f107ffab2827ce5d7307a
Mat4539 <mborg0020@gmail.com>
2023-04-11 21:10:23 -0400

    Added include

--- SkyFinder/urls.py
+++ SkyFinder/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
 
 from SkyFinderWebApp.views import home, track, contact
